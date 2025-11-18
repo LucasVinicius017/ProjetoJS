40343ee8d868380dc197cc1ab79054c210d75730
Lucas Vinicius dos Santos <lucassantosvini17@gmail.com>
2025-11-18 17:19:25 -0300

    Código finalizado

--- AULA-JS-TABUADA-SALA/assets/js/script.js
+++ AULA-JS-TABUADA-SALA/assets/js/script.js
@@ -6,9 +6,26 @@ function gerarATabuada ()
     const entradaNumero = document.getElementById("entradaNumero");
     const resultado = document.getElementById("resultado");
 
-    let numero = entradaNumero.value;
+    //Limpar o resultado anterior
 
-    
+    let numero = parseInt(entradaNumero.value);
+
+    //Validação do número coletado da console que foi informado pelo usuário
+    if(isNaN(numero) || numero < 1 || numero > 10)
+    {
+        resultado.innerText = "Número informado , Inválido. Verifique!";
+        return;
+    }
+
+    //Geração da tabuada solicitada a partir do número
+    for(let contador=1; contador<=10; contador++)
+    {
+        const linha = document.createElement("p");
+        linha.textContent = `${numero} x ${contador} = ${numero * contador}`;
+
+        resultado.appendChild(linha);
+        
+    }
 }
 
 botaoGerar.addEventListener("click", gerarATabuada);
