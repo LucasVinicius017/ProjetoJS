7fdf7ef6900df738c71537ee8ddd7ed72174d8f1
Lucas Vinicius dos Santos <lucassantosvini17@gmail.com>
2025-11-18 15:27:42 -0300

    Update script.js

---
+++ AULA-JS-TABUADA-SALA/assets/js/script.js
@@ -0,0 +1,14 @@
+
+const botaoGerar = document.getElementById("botaoGerar");
+
+function gerarATabuada ()
+{
+    const entradaNumero = document.getElementById("entradaNumero");
+    const resultado = document.getElementById("resultado");
+
+    let numero = entradaNumero.value;
+
+    
+}
+
+botaoGerar.addEventListener("click", gerarATabuada);
