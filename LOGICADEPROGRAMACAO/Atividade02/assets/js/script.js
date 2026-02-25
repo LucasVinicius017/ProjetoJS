ad209be9ab5b89a15d8073087e23f08d493fac37
Lucas Vinicius dos Santos <lucassantosvini17@gmail.com>
2026-02-25 14:47:55 -0300

    Atualizado

--- LOGICADEPROGRAMACAO/Atividade02/assets/js/script.js
+++ LOGICADEPROGRAMACAO/Atividade02/assets/js/script.js
@@ -1,22 +1,27 @@
 const nomeJs = document.getElementById("Nome");
 const salarioJs = document.getElementById("Salario");
 const totalvendasJs = document.getElementById("TotalVendas");
-const comissaoJs = 7.5/100
+const calcularJs = document.getElementById("Calcular");
 const resultadoJs = document.getElementById("Resultado");
 
- 
 function Atividade02()
 {
-    const paragrafo1 = createElement("p");
+    let comissaoJs = parseFloat(totalvendasJs.value) * 0.75;
+    const paragrafo1 = document.createElement("p");
     paragrafo1.innerText = `O nome do vendendor é ${nomeJs.value}`;
     resultadoJs.appendChild(paragrafo1);
-    const paragrafo2 = createElement("p");
+
+    const paragrafo2 = document.createElement("p");
     paragrafo2.innerText = `Salário do vendedor será ${salarioJs.value}`;
     resultadoJs.appendChild(paragrafo2);
-    const paragrafo3 = createContext("p");
+
+    const paragrafo3 = document.createElement("p");
     paragrafo3.innerText = `Total de Vendas é ${totalvendasJs.value}`;
     resultadoJs.appendChild(paragrafo3);
- 
- 
+
+    const paragrafo4 = document.createElement("p");
+    paragrafo4.innerText = `Valor da comissão é ${comissaoJs}`
+    resultadoJs.appendChild(paragrafo4);
+
 }
-botaoOk.addEventListener("click", Atividade01);
+calcularJs.addEventListener("click", Atividade02);
